51c202edd3bf9b602f4e7fe8cd6d976d6035b6cc
DrDiban <51023031+DrDiban@users.noreply.github.com>
2021-05-27 17:26:32 +0800

    Update covid_crossfilter.js

--- covid_crossfilter.js
+++ covid_crossfilter.js
@@ -153,7 +153,7 @@ d3.csv(window.CrossFilter3.config.dataUrl,  function (data) {
 		.elasticY(true)
 		.xAxisLabel('Dates')
 		.yAxisLabel('Covid-19 deaths')
-		.ordinalColors(['#fc2e1c'])
+		.ordinalColors(['#9e1000'])
 		.xAxis().ticks(d3.time.months, 1)
 		.tickFormat(d3.time.format("%b,%Y"))
 
